94654036b137495e1a4465fbfda2918085e93def
Owen Herndon <owenherndon@Owens-MacBook-Pro.local>
2016-10-22 10:10:56 -0500

    logic works, converting to iife structure

--- javascripts/eventhandle.js
+++ javascripts/eventhandle.js
@@ -1,8 +1,39 @@
-// var CarLot = (function (activateEvents) {
- 		
- 		//onclick for card selected
- 		//onkeystroke for enter on input box
+var TankLot = (function (newTankLot) {
+ 	
+ 	newTankLot.tankEvents = function(){	
+		var selectedTankBio = null;
+		var userInput = document.getElementById("userInput");
+		var containerEl = document.getElementsByClassName("tank_container");
 
-//   };
+		for (var i = 0; i < containerEl.length; i++) {
+  				containerEl[i].addEventListener("click", function(event) {
+    			for (var x = 0; x < containerEl.length; x++) {
+      				containerEl[x].classList.remove('borderLarge');
+    			}
+    			event.currentTarget.classList.add('borderLarge');
+    			selectedTankBio = event.currentTarget;
+    			userInput.value = "";
+    			userInput.focus();
+    			userInput.value = selectedTank.querySelector('.description').innerHTML;
+  			});
+		}
 
-// })();
+		document.getElementById('userInput').addEventListener('keyup', function(e) {
+  			if (selectedTankBio) {
+    			selectedTankBio.querySelector('.description').innerHTML = this.value;
+  			}
+		});
+
+		document.getElementById('userInput').addEventListener('keypress', function(e) {
+  			if (e.keyCode == 13) {
+    			selectedTankBio.classList.remove('.borderLarge');
+    			selectedTankBio = null;
+    			this.value = "";
+    			return false;
+  			}
+  		});
+	}
+	
+	return newTankLot;
+
+})(TankLot || {});
--- javascripts/loadinvt.js
+++ javascripts/loadinvt.js
@@ -1,6 +1,4 @@
-var TankLot = (function () {
-  
-  //var inventory = [];
+var TankLot = (function (LoadTankLot) {
 
   	console.log("test");
 
@@ -23,16 +21,6 @@ var TankLot = (function () {
 
   		for (var i = 0; i < tankData.length; i++) {
   			currentTank = tankData[i];
-  			// productContainer.innerHTML += `<div class="container">`
-  			// productContainer.innerHTML += `<div class="col-xs-6 col-md-4">`
-  			// productContainer.innerHTML += `<img src="${currentTank.jpg}"><br>`
-  			// productContainer.innerHTML += `${currentTank.make}<br>`
-  			// productContainer.innerHTML += `${currentTank.model}<br>`
-  			// productContainer.innerHTML += `${currentTank.year}<br>`
-  			// productContainer.innerHTML += `${currentTank.price}<br>`
-  			// productContainer.innerHTML += `${currentTank.description}`
-  			// productContainer.innerHTML += `</div>`
-  			// productContainer.innerHTML += `</div>`
   			productContainer.innerHTML += 
   				`<div id="container" class="col-xs-6 col-md-4 tank_container">
   				<img src="${currentTank.jpg}">
@@ -42,29 +30,12 @@ var TankLot = (function () {
   				<p>$${currentTank.price}</p>
   				<div class="description">${currentTank.description}</div></div>`
 
-  		}	
-  		//domDisplay();
+  		}
+  		LoadTankLot.tankEvents();
+
   	}
 
   	var output = document.getElementById('storefront');
-  	
-  // 	function domDisplay(){
-  // 		output.innerHTML = "";
-
-		// var productContainer = document.getElementById("storefront");
-  		
-  // 		for (var i = 0; i < tanks.length; i++) {
-  // 			productContainer.innerHTML += `<div class="container">`
-  // 			productContainer.innerHTML += `<div class="col-xs-6 col-md-4">`
-  // 			productContainer.innerHTML += `<img src="${tanks[i].jpg}">`
-  // 			productContainer.innerHTML += `${tanks[i].make}`
-  // 			productContainer.innerHTML += `${tanks[i].model}`
-  // 			productContainer.innerHTML += `${tanks[i].year}`
-  // 			productContainer.innerHTML += `${tanks[i].price}`
-  // 			productContainer.innerHTML += `${tanks[i].description}`
-  // 			productContainer.innerHTML += `</div>`
-  // 		}	
-  // 	}
 
   	var myRequest = new XMLHttpRequest();
 
@@ -74,46 +45,6 @@ var TankLot = (function () {
 	myRequest.open("GET", "inventory.json");
 	myRequest.send();
 
-	//var selectedContainer = document.getElementById("container");
-	var selectedTankBio = null;
-	var userInput = document.getElementById("userInput");
-	var containerEl = document.getElementsByClassName("tank_container");
+  return LoadTankLot;
 
-	for (var i = 0; i < containerEl.length; i++) {
-  		containerEl[i].addEventListener("click", function(event) {
-    		for (var x = 0; x < containerEl.length; x++) {
-      			containerEl[x].classList.remove('borderLarge');
-    		}
-    		event.currentTarget.classList.add('borderLarge');
-    		selectedTankBio = event.currentTarget;
-    		userInput.focus();
-    		userInput.value = selectedTank.querySelector('.description').innerHTML;
-  		});
-	}
-
-	document.getElementById('useInput').addEventListener('keyup', function(e) {
-  		if (selectedTankBio) {
-    		selectedTankBio.querySelector('.description').innerHTML = this.value;
-  		}
-	});
-
-	document.getElementById('userInput').addEventListener('keypress', function(e) {
-  		if (e.keyCode == 13) {
-    		selectedTankBio.classList.remove('.borderLarge');
-    		selectedTankBio = null;
-    		this.value = "";
-    		return false;
-  		}
-  	});
-
-  return {
-    // loadInventory: function (callback) {
-    //   var inventoryLoader = new XMLHttpRequest();
-
-    //   inventoryLoader.addEventListener("load", function () {
-
-    //   });
-    // }
-  };
-
-})();
+})(TankLot || {});
